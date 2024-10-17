3b680fffe0a6b3c4c0eab7b11f811892e04391cc
Marcos Pereira <marcos.p.10@hotmail.com>
2024-10-17 14:16:45 -0300

    atualização driver chrome

--- src/services/betano_bot_service.py
+++ src/services/betano_bot_service.py
@@ -4,11 +4,15 @@ import time
 
 class BetanoBotService:
     def __init__(self, url: str = "https://br.betano.com"):
+        self.options = uc.ChromeOptions()
+        self.options.headless = True
+
         self.driver = uc.Chrome(
+            use_subprocess=True,
             headless=True,
-            use_subprocess=False,
-            version_main=127,
+            # options=self.options,
         )
         self.driver.get(url)
+        time.sleep(20)
         self.driver.save_screenshot('nowsecure.png')
         time.sleep(10)
